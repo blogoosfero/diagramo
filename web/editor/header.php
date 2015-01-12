d59206a22bf48ef513a1b14882df55650d0ba6ee
dtygel <dtygel@gmail.com>
2015-01-12 19:33:01 -0200

    Option to remove header

--- web/editor/header.php
+++ web/editor/header.php
@@ -16,6 +16,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+if ($_REQUEST['noHeader'])
+    exit;
+
 $WEBADDRESS = $delegate->settingsGetByKeyNative('WEBADDRESS');
 ?>
 <div id="header">    
