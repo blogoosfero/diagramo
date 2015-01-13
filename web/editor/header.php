3c1ec9d06fbb53bdb24bd903fe58ca16e572ae29
dtygel <dtygel@gmail.com>
2015-01-13 16:50:37 -0200

    Use setting SHOW_HEADER to decide if header menu will be shown

--- web/editor/header.php
+++ web/editor/header.php
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-if (!isset($_REQUEST['noHeader'])) {
+if (SHOW_HEADER) {
 
     $WEBADDRESS = $delegate->settingsGetByKeyNative('WEBADDRESS');
     ?>
